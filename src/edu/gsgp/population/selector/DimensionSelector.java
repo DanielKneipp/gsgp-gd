7fef2a2d3d712822ccd97ace52055633a5c34823
bruno <bruno@linux-PC>
2016-10-06 12:49:42 -0300

    Structure to new selection's operator.

---
+++ src/edu/gsgp/population/selector/DimensionSelector.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.gsgp.population.selector;
+
+import edu.gsgp.population.Individual;
+import edu.gsgp.population.Population;
+import edu.gsgp.utils.MersenneTwister;
+
+/**
+ *
+ * @author bruno
+ */
+public class DimensionSelector implements IndividualSelector{
+
+    public DimensionSelector(){
+        
+    }
+    
+    @Override
+    public Individual selectIndividual(Population population, Individual individual, MersenneTwister rnd) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
